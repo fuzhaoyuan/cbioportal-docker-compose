55ac5d88d34bbb0040aa6c55c3070804a815c598
Ryan Fu <fuzhaoyuan@gmail.com>
2023-03-06 16:42:34 -0500

    Integrate arm images into arm64 yaml configuration file

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@ done
 
 # add override docker file for arm64
 # see https://github.com/cBioPortal/cbioportal/issues/9829
-if [[ ! -f "docker-compose.override.yml" ]] && [[ "$(arch)" = "arm64" ]]; then
+if [[ ! -f "docker-compose.override.yml" ]] && [[ "$(uname -s)" == "Darwin" ]] && [[ "$(sysctl -n machdep.cpu.brand_string)" =~ M.* ]]; then
     cp docker-compose.arm64.yml docker-compose.override.yml
 fi
